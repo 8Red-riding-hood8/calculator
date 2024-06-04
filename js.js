77df7201c3d24b7d38aa75955c363f6ee7a86dec
8Red-riding-hood8 <denysuikmaria@gmail.com>
2024-06-04 20:06:06 +0300

    Create calculator

---
+++ js.js
@@ -0,0 +1,25 @@
+let option = prompt("Enter option(add, sub, mult, div):", 'add');
+let firstNum = +prompt("First number:", 1);
+let secondNum = +prompt("Second number:", 1);
+let result;
+switch(option){
+    case "add":
+        result = firstNum+secondNum;
+        document.write(`${firstNum} + ${secondNum} = ${result}`)
+        break;
+    case "sub":
+        result = firstNum - secondNum;
+        document.write(`${firstNum} - ${secondNum} = ${result}`)
+        break;
+    case "mult":
+        result = firstNum * secondNum;
+        document.write(`${firstNum} * ${secondNum} = ${result}`)
+        break;
+    case "div":
+        result = firstNum / secondNum;
+        document.write(`${firstNum} / ${secondNum} = ${result}`)
+        break;
+    default:
+        console.log("incorrect option");
+        break;
+}
